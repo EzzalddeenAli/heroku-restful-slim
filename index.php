bca01393954bac82b49f0e19877f8bd961a81286
nakaikaz <nakaikaz@gmail.com>
2016-03-16 19:13:46 +0900

    added database

--- index.php
+++ index.php
@@ -4,10 +4,44 @@ require 'vendor/autoload.php';
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-$app = new \Slim\App;
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$config = array(
+	'displayErrorDetails' => true,
+	'db' => array(
+		'host' => $dbopts['host'],
+		'dbname' => ltrim($dbopts['path'], '/'),
+		'user' => $dbopts['user'],
+		'pass' => $dbopts['pass'],
+		'port' => $dbopts['port']
+	)
+);
+$container = new \Slim\Container(array(
+	'settings' => $config
+));
+$container['db'] = function($c){
+	$db = $c['settings']['db'];
+	return new medoo(array(
+		'database_type' => 'pgsql',
+		'database_name' => $db['dbname'],
+		'server' => $db['host'],
+		'username' => $db['user'],
+		'password' => $db['pass'],
+		'port' => $db['port'],
+		'charset' => 'utf8'
+	));
+};
+$app = new \Slim\App($container);
 
 $app->get('/', function(Request $req, Response $res){
 	echo 'hello heroku-restful-slim';
+});
+
+$app->get('/db', function(Request $req, Response $res){
+	$db = $this->get('db');
+	$data = $db->select('users', '*');
+	echo '<pre>';
+	var_dump($data);
+	echo '</pre>';
 });
 
 $app->run();
